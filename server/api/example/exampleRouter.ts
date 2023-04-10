f8eb54a69fd862c277dc572d1c7b2f947e20995f
Day Meijroos <pekieend@gmail.com>
2023-04-10 15:48:47 +0200

    🧹: Remove unnecessary things in router

--- server/api/example/exampleRouter.ts
+++ server/api/example/exampleRouter.ts
@@ -5,8 +5,6 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
-const ee = new EventEmitter();
-
 export const exampleRouter = createTRPCRouter({
   posts: publicProcedure
     .input(z.number().optional())
@@ -31,21 +29,6 @@ export const exampleRouter = createTRPCRouter({
 
         },
       });
-      ee.emit('add', post);
       return post;
-    }),
-  onPost: publicProcedure.subscription(() => {
-    return observable<Post>((emit) => {
-      const onAdd = (data: Post) => {
-        // emit data to client
-        emit.next(data);
-      };
-      // trigger `onAdd()` when `add` is triggered in our event emitter
-      ee.on('add', onAdd);
-      // unsubscribe function when client disconnects or stops subscribing
-      return () => {
-        ee.off('add', onAdd);
-      };
-    });
-  }),
+    })
 });

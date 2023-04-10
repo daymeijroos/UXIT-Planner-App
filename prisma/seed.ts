6f9ea0c96c5171aea1284878a57df8f647e0ecf7
Day Meijroos <pekieend@gmail.com>
2023-04-10 13:39:51 +0200

    fix seeding issue

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,36 +5,36 @@ const prisma = new PrismaClient()
 //use createmany to create a bunch of posts filled with mock filler posts with random events
 
 async function posts() {
-  await prisma.post.create({
-    data: {
-      title: 'Im feeling good, went for some ice-cream',
-      content: 'Today was a good day, I went for some ice-cream and it was delicious',
-    }
-  })
-  await prisma.post.create({
-    data: {
-      title: 'I went to the gym',
-      content: 'Today I went to the gym and I did some squats',
-    }
-  })
-  await prisma.post.create({
-    data: {
-      title: 'Did some coding',
-      content: 'Today I did some coding and it was fun',
-    }
-  })
   // await prisma.post.create({
   //   data: {
-  //     title: 'I went for a walk',
-  //     content: 'Today I went for a walk and it was nice',
+  //     title: 'Im feeling good, went for some ice-cream',
+  //     content: 'Today was a good day, I went for some ice-cream and it was delicious',
   //   }
   // })
   // await prisma.post.create({
   //   data: {
-  //     title: 'I went to the beach',
-  //     content: 'Today I went to the beach and it was sunny',
+  //     title: 'I went to the gym',
+  //     content: 'Today I went to the gym and I did some squats',
   //   }
   // })
+  // await prisma.post.create({
+  //   data: {
+  //     title: 'Did some coding',
+  //     content: 'Today I did some coding and it was fun',
+  //   }
+  // })
+  // // await prisma.post.create({
+  // //   data: {
+  // //     title: 'I went for a walk',
+  // //     content: 'Today I went for a walk and it was nice',
+  // //   }
+  // // })
+  // // await prisma.post.create({
+  // //   data: {
+  // //     title: 'I went to the beach',
+  // //     content: 'Today I went to the beach and it was sunny',
+  // //   }
+  // // })
 }
 
 posts().then(async () => {

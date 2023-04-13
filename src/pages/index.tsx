7143b9378d351c84ee862de3e92a368b5fb0f9b8
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-04-13 17:38:27 +0200

    feat: added navigation bar with buttons from Di to Zo, each navigating the user to the respective page. Pages have no content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,32 @@
-import TrpcExample from '../components/example/trpc.example'
-import AuthExample from '../components/example/auth.example'
-import LoginExample from '../components/example/login.example'
+// import TrpcExample from '../components/example/trpc.example'
+// import AuthExample from '../components/example/auth.example'
+// import LoginExample from '../components/example/login.example'
+//
+// export default function Home() {
+//   return (
+//     <div className="flex flex-col lg:flex-row justify-around min-h-screen bg-amber-200 items-center">
+//       <div className="flex flex-col gap-2 w-72">
+//         <h1 className="text-6xl font-bold text-blue-800">
+//           Welcome to <a href="https://nextjs.org" className="text-blue-600 underline">Next.js!</a>
+//         </h1>
+//         <LoginExample/>
+//       </div>
+//       <div className="flex flex-col gap-2">
+//         <TrpcExample/>
+//         <AuthExample/>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default function Home() {
+import Navigation from "../components/aria/navigation";
+
+const Home = () => {
   return (
-    <div className="flex flex-col lg:flex-row justify-around min-h-screen bg-amber-200 items-center">
-      <div className="flex flex-col gap-2 w-72">
-        <h1 className="text-6xl font-bold text-blue-800">
-          Welcome to <a href="https://nextjs.org" className="text-blue-600 underline">Next.js!</a>
-        </h1>
-        <LoginExample/>
-      </div>
-      <div className="flex flex-col gap-2">
-        <TrpcExample/>
-        <AuthExample/>
-      </div>
+    <div>
+      <Navigation/>
     </div>
-  )
-}
+  );
+};
+
+export default Home;

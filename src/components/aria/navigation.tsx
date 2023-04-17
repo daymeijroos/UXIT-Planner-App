5cce21f353edfa3b05f8c737808d0e4bb4ea54b5
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-04-17 23:36:31 +0200

    styling: changed navigation.tsx to follow style guide, navigation bar now has a white background and buttons are dark-teal

--- src/components/aria/navigation.tsx
+++ src/components/aria/navigation.tsx
@@ -1,6 +1,7 @@
 import { useButton } from "@react-aria/button";
 import { useFocusRing } from "@react-aria/focus";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navigation = () => {
   return (
@@ -8,39 +9,51 @@ const Navigation = () => {
   );
 };
 
-const NavigationButton = ({ day, linkTo }) => {
+const NavigationButton = ({ day, linkTo, selected }) => {
   const { buttonProps, isPressed } = useButton({
     onPress: () => console.log(day),
   });
   const { isFocusVisible, focusProps } = useFocusRing();
 
+  const isCurrentDay = linkTo === selected;
+
   return (
     <Link href={linkTo}>
       <div
         className={`${
-          isPressed ? "bg-blue-400" : "bg-blue-500"
-        } text-white rounded-full h-12 w-12 flex items-center justify-center focus:outline-none focus:ring-4 ${
+          isPressed ? "bg-blue-400" : "bg-white"
+        } text-black rounded-full h-12 w-12 flex flex-col items-center justify-center focus:outline-none focus:ring-4 ${
           isFocusVisible ? "ring-blue-300" : ""
+        } ${
+          isCurrentDay
+            ? "bg-dark-teal"
+            : isPressed
+              ? "bg-dark-teal"
+              : "bg-white text-gray-700"
         }`}
         {...buttonProps}
         {...focusProps}
       >
         <span className="sr-only">{day}</span>
-        <span>{day.slice(0, 2)}</span>
+        <span className="text-lg font-bold">{day.slice(0, 2)}</span>
+        <span className="text-sm">{new Date(day).getDate()}</span>
       </div>
     </Link>
   );
 };
 
 const NavigationBar = () => {
+  const router = useRouter();
+  const selected = router.pathname;
+
   return (
-    <nav className="bg-gray-200 py-2 px-4 rounded-lg flex space-x-2 justify-center">
-      <NavigationButton day="Dinsdag" linkTo="/rooster/dinsdag" />
-      <NavigationButton day="Woensdag" linkTo="/rooster/woensdag" />
-      <NavigationButton day="Donderdag" linkTo="/rooster/donderdag" />
-      <NavigationButton day="Vrijdag" linkTo="/rooster/vrijdag" />
-      <NavigationButton day="Zaterdag" linkTo="/rooster/zaterdag" />
-      <NavigationButton day="Zondag" linkTo="/rooster/zondag" />
+    <nav className="bg-white py-2 px-4 rounded-lg flex space-x-2 justify-center">
+      <NavigationButton day="Dinsdag 2023-04-18" linkTo="/rooster/dinsdag" selected={selected} />
+      <NavigationButton day="Woensdag 2023-04-19" linkTo="/rooster/woensdag" selected={selected} />
+      <NavigationButton day="Donderdag 2023-04-20" linkTo="/rooster/donderdag" selected={selected} />
+      <NavigationButton day="Vrijdag 2023-04-21" linkTo="/rooster/vrijdag" selected={selected} />
+      <NavigationButton day="Zaterdag 2023-04-22" linkTo="/rooster/zaterdag" selected={selected} />
+      <NavigationButton day="Zondag 2023-04-23" linkTo="/rooster/zondag" selected={selected} />
     </nav>
   );
 };

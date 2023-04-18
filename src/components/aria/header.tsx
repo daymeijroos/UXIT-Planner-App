dde865041ec2b1ccbdcf37697167e8c79b2ec2a8
wardalbam <wardalbam32@gmail.com>
2023-04-18 20:45:52 +0200

    schedule overview afgemaakt

--- src/components/aria/header.tsx
+++ src/components/aria/header.tsx
@@ -6,11 +6,11 @@ const header = () => {
   const router = useRouter();
   const weekday = ["zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag"];
   return (
-    <nav className="fixed bottom-0 bg-gray-0 text-black py-3 px-6 flex justify-between items-center w-screen">
+    <nav className="fixed bottom-0 bg-gray-0 text-black py-3 px-6 flex justify-around items-center w-screen">
       <button className="headerNavButton text-lg font-medium border-1-black border border-indigo-600" type="button" onClick={() => router.push('/notifications')} >
         <Bell className='stroke-1' color='#000' size="24"  width="30" height="30" />
       </button>
-      <button className="headerNavButton text-lg font-medium border border-indigo-600" type="button" 
+      <button className="headerNavButton headerNavButton_active text-lg font-medium border border-indigo-600" type="button" 
         onClick={() => router.push(
         '/rooster/'+ weekday[new Date().getDay()]
         )}>

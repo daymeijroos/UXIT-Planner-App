dc36ec03b1ef8211c814199bc9cc69fd984fcc45
wardalbam <wardalbam32@gmail.com>
2023-04-18 19:36:25 +0200

    test commit

--- src/components/aria/schedule.tsx
+++ src/components/aria/schedule.tsx
@@ -2,65 +2,31 @@ import React from 'react';
 import { useRouter } from 'next/navigation';
 //  import StaffingCard
  import { StaffingCard } from "../../components/overview-components/staffing-card";
+import { api } from '../../utils/api';
 
 
 const schedule = () => {
   const router = useRouter();
+
+ 
+
+  const staffings = api.staffing.getStaffing.useQuery();
+
+  if (staffings.isLoading) {
+    return <div>loading...</div>;
+  }
+
+  if (staffings.error) {
+    return <div>{staffings.error.message}</div>;
+  }
+
   return (
+
     <div className='overflow-y-auto h-[70vh]'>
       
-      <StaffingCard staffing={{
-        shift: {
-          id: "1",
-          // start and end are type Date 
-          start: new Date(),
-          end: new Date(),
-          note: "test",
-          staffings: [
-            {
-              user: {
-                id: "1",
-                first_name: "Niek"
-              },
-              shift_type: {
-                id: "1",
-                name: "balie"
-              }
-            },
-            {
-              user: {
-                id: "2",
-                first_name: "Henk"
-              },
-              shift_type: {
-                id: "2",
-                name: "zaal"
-              }
-            },
-            {
-              user: {
-                id: "3",
-                first_name: "Laura"
-              },
-              shift_type: {
-                id: "2",
-                name: "zaal"
-              }
-            }
-          ]
-        },
+      <StaffingCard staffing={staffings.data[0]} />
 
-        shift_type: {
-          id: "1",
-          name: "balie"
-        },
-        user: {
-          id: "1",
-          first_name: "Niek"
-        }
-      }} />
-
-      <StaffingCard staffing={{
+      {/* <StaffingCard staffing={{
               shift: {
                 id: "1",
                 // start and end are type Date 
@@ -208,7 +174,7 @@ const schedule = () => {
                 id: "1",
                 first_name: "Niek"
               }
-      }} />
+      }} /> */}
 
     </div>
 

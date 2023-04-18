542b73f7928f8512bfc8d257c097cf387a8a06b4
wardalbam <wardalbam32@gmail.com>
2023-04-18 21:35:33 +0200

    rooster index pagina toegevoegd, en dagen bestanden verwijderd.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,71 +1,16 @@
-import { Button } from "../components/aria/button";
-import { Card } from "../components/aria/card";
+import Header from "../components/aria/header";
 import Navigation from "../components/aria/navigation";
-import { StaffingCard } from "../components/overview-components/staffing-card";
-import { Zap } from "react-feather";
-import { api } from "../utils/api";
+import Schedule from "../components/aria/schedule";
 
-const Home = () => {
-  const context = api.useContext();
-  const { mutate: generateSchedule } = api.schedule.generate.useMutation({onSuccess: () => {
-    context.staffing.getStaffing.invalidate().catch((error) => {
-    console.error(error);
-  })}});
+const index = () => {
 
   return (
     <div>
-      <Button fillWidth color="success" onPress={() => {generateSchedule()}}>
-        <b>Generate schedule</b><Zap className="ml-2"/>
-      </Button>
-      <Navigation/>
-
-      <Card button buttonText="dissmisss" buttonColor="success">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <p className="text-gray-500">This is a card</p>
-      </Card>
-      <StaffingCard staffing={{
-        //Hier kan een staffing van de staffing endpoint in. Deze zijn compatibel met de staffing type van de staffing card
-        id: "1",
-        shift_id: "1",
-        shift: {
-          id: "1",
-          start: new Date(),
-          end: new Date(new Date().setTime(new Date().getTime() + 10000000)),
-          staffings: [
-            {
-              user: {
-                id: "1",
-                first_name: "Henk",
-              },
-              shift_type: {
-                id: "1",
-                name: "Balie",
-                description: "balie shift jawel",
-              },
-            },
-            {
-              user: {
-                id: "2",
-                first_name: "Laura",
-              },
-              shift_type: {
-                id: "2",
-                name: "Zaal",
-                description: "balie shift jawel",
-              },
-            },
-          ],
-        },
-        shift_type_id: "1",
-        shift_type: {
-          id: "1",
-          name: "balie",
-          description: "test",
-        },
-        user_id: "2",
-      }}/>
+      <Navigation />
+      <Schedule />
+      <Header />
     </div>
   );
 };
 
-export default Home;
+export default index;

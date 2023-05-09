e82f9002f2a2acc3a588fb5cd8210ae18f0ce682
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-05-09 20:30:42 +0200

    feat: added admin button to navigation bar

--- src/components/elements/navigation-bar.tsx
+++ src/components/elements/navigation-bar.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Bell, Calendar, User } from 'react-feather';
-import { useRouter } from 'next/navigation';
-import { Button } from "../atoms/button"
-import { api } from '../../utils/api';
+import React from "react";
+import { Bell, Calendar, User, Tool } from "react-feather";
+import { useRouter } from "next/navigation";
+import { Button } from "../atoms/button";
+import { api } from "../../utils/api";
 
 export const NavigationBar = () => {
 
@@ -10,14 +10,17 @@ export const NavigationBar = () => {
   const router = useRouter();
   return (
     <nav className="fixed bottom-0 bg-gray-0 text-black py-3 px-6 flex items-center w-screen justify-center space-x-4">
-      <Button aria-label="Notificaties" title='Notificaties'>
-        <Bell className='stroke-2' color='#000' size="24" width="30" height="30" />
+      <Button aria-label="Notificaties" title="Notificaties">
+        <Bell className="stroke-2" color="#000" size="24" width="30" height="30" />
       </Button>
-      <Button color='success' onPress={() => router.push('/')} aria-label="Kalender" title='Kalender'>
-        <Calendar className='stroke-2' color='#000' size="24" width="30" height="30" />
+      <Button color="success" onPress={() => router.push("/")} aria-label="Kalender" title="Kalender">
+        <Calendar className="stroke-2" color="#000" size="24" width="30" height="30" />
       </Button>
-      <Button onPress={() => router.push('/admin')} aria-label="Account" title='Account'>
-        <User className='stroke-2' color='#000' size="24" width="30" height="30" />
+      <Button aria-label="Account" title="Account">
+        <User className="stroke-2" color="#000" size="24" width="30" height="30" />
+      </Button>
+      <Button onPress={() => router.push("/admin")} aria-label="Admin" title="Admin">
+        <Tool className="stroke-2" color="#000" size="24" width="30" height="30" />
       </Button>
     </nav>
   );

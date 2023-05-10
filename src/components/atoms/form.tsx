b508dea09d8766bba3a438c443bd555311c26f1e
AbrahamLincoln <s1136731@student.hsleiden.nl>
2023-05-10 20:33:40 +0200

    Added inwerken

---
+++ src/components/atoms/form.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import {Button} from "./button";
+import {TextField} from "./text-field";
+
+export function FormPreset() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: ''
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: ''
+  });
+
+  const handleFirstNameChange = (value: string) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      firstName: value
+    }) as typeof formData);
+  };
+
+  const handleLastNameChange = (value: string) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      lastName: value
+    }) as typeof formData);
+  };
+
+  const handleEmailChange = (value: string) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      email: value
+    }) as typeof formData);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const errors = validateForm();
+    setFormErrors(errors);
+
+    if (Object.keys(errors).length === 0) {
+      // Form is valid, submit data or perform other actions
+      console.log(formData);
+    }
+  };
+
+  const validateForm = () => {
+    const errors: { [key: string]: string } = {};
+
+    if (!formData.firstName) {
+      errors.firstName = 'Vul uw voornaam in';
+    }
+
+    if (!formData.lastName) {
+      errors.lastName = 'Vul uw achternaam in';
+    }
+
+    if (!formData.email) {
+      errors.email = 'Vul uw e-mail in';
+    } else if (!isValidEmail(formData.email)) {
+      errors.email = 'Vul een geldige e-mail in';
+    }
+
+    return errors;
+  };
+
+  const isValidEmail = (email: string) => {
+    // Add your email validation logic here
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  return (
+      <div className="max-w-md mx-auto p-4">
+        <h1 className="text-2xl font-bold text-gray-800 mb-4">Inwerken vrijwilliger</h1>
+        <form onSubmit={handleSubmit}>
+          <TextField label="Voornaam" error={formErrors.firstName} onChange={handleFirstNameChange}></TextField>
+          <TextField label="Achternaam" error={formErrors.lastName} onChange={handleLastNameChange}></TextField>
+          <TextField label="E-mail" type= "email" error={formErrors.email} onChange={handleEmailChange}></TextField>
+        <Button type="submit">Indienen</Button>
+      </form>
+      </div>
+  );
+}
---
+++ src/pages/OrientNewWorker.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {TextField} from "../components/atoms/text-field";
+import {NavigationBar} from "../components/elements/navigation-bar";
+import {FormPreset} from "../components/atoms/form";
+
+
+const OrientNewWorker = () => {
+    return (
+        <div>
+            {<FormPreset></FormPreset>}
+        </div>
+    );
+};
+
+export default OrientNewWorker;

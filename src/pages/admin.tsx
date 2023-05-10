32d07b797435106081d0526561da4d5d13b61b01
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-05-10 22:57:28 +0200

    fix: repaired responsiveness at certain resolutions

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -36,21 +36,21 @@ const Admin = () => {
     <div className="flex flex-col items-center justify-center w-full h-full">
       <h1 className="mt-4 text-3xl font-bold text-center">Admin paneel</h1>
       <h2 className="mt-4 text-xl font-bold text-center">Rooster</h2>
-      <div className="flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
+      <div className="sm:flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
         <Button color="success" onPress={() => { void generateSchedule(); }} className="w-64">Genereer Rooster</Button>
         <Button color="error" onPress={() => { void removeStaffings(); }} className="w-64 max-sm:mt-4">Verwijder Rooster</Button>
       </div>
-      <div className="flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
+      <div className="sm:flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
         <Button className="w-64">Handmatige aanpassingen</Button>
         <Button className="w-64 max-sm:mt-4">Openingsweekend aangeven</Button>
       </div>
-      <div className="flex flex-col items-center m-4 space-y-4 justify-center">
+      <div className="sm:flex flex-col items-center m-4 space-y-4 justify-center">
         <h2 className="text-xl font-bold text-center">Vrijwilligers</h2>
-        <div className="flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
+        <div className="sm:flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
           <Button className="w-64">Voorkeur aanpassen</Button>
           <Button className="w-64 max-sm:mt-4">Beschikbaarheid aanpassen</Button>
         </div>
-        <div className="flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
+        <div className="sm:flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
           <Button className="w-64">Werkuren aanpassen</Button>
           <Button className="w-64 max-sm:mt-4">Verlof aangeven</Button>
         </div>
@@ -62,9 +62,9 @@ const Admin = () => {
           <Button className="w-64">Nieuw shifttype toevoegen</Button>
         </div>
         <h2 className="mt-4 text-xl font-bold text-center">Accounts beheren</h2>
-        <div className="flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
+        <div className="sm:flex flex-wrap mt-4 space-x-4 max-sm:space-x-0 justify-center">
           <Button className="w-64">Rollenbeheer</Button>
-          <Button className="w-64">Account aanmaken</Button>
+          <Button className="w-64 max-sm:mt-4 max-sm:mb-20">Account aanmaken</Button>
         </div>
       </div>
   {unfulfilledShifts.data?.map((request) => {

b9e327267c0cf73e566422db011c87f375f7af30
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-05-11 13:01:52 +0200

    fix: added white background to nav bar to prevent overlapping issues

--- src/components/elements/navigation-bar.tsx
+++ src/components/elements/navigation-bar.tsx
@@ -9,7 +9,7 @@ export const NavigationBar = () => {
 
   const router = useRouter();
   return (
-    <nav className="fixed bottom-0 bg-gray-0 text-black py-3 px-6 flex items-center w-screen justify-center space-x-4">
+    <nav className="fixed bottom-0 bg-white text-black py-3 px-6 flex items-center w-screen justify-center space-x-4">
       <Button aria-label="Notificaties" title="Notificaties">
         <Bell className="stroke-2" color="#000" size="24" width="30" height="30" />
       </Button>

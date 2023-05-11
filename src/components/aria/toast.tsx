ddbabf8363bb45a7cfe8fc2d17e4842f33ad6527
Moatasem Alhumsi Alrefaai <54780624+mhtsm2001@users.noreply.github.com>
2023-05-11 13:59:40 +0200

    werkende versie 1.0

---
+++ src/components/aria/toast.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {useState} from "react";
+import { AlertOctagon, X } from 'react-feather';
+import { useFocusRing } from "@react-aria/focus";
+import fa from "@faker-js/faker/locales/fa";
+import { set } from "zod";
+
+interface ToastProps {
+  title: string;
+  msg: string;
+  type: number;
+
+  seconds?: number;
+}
+
+
+
+const Toast: React.FC<ToastProps> = ({children, title ,msg, type, seconds}) => {
+
+  const [shown, setShown] = useState(true);
+  const [opacity, setOpacity] = useState(false);
+  const [up, setUp] = useState(false);
+
+  if (seconds != undefined){
+    setTimeout(() =>{
+      setUp(true);
+    },seconds/1.02)
+  }
+
+  if (seconds != undefined){
+    setTimeout(() =>{
+      setOpacity(true);
+    },seconds/1.07)
+  }
+
+  if (seconds != undefined){
+    setTimeout(() =>{
+      setOpacity(false);
+      setShown(false);
+    },seconds)
+  }
+  function handleClick(){
+    setShown(false)
+  }
+
+
+
+  return (
+    <>
+      {shown && (
+      <div className={`absolute w-96 bottom-5 right-5 h-1/5 bg-black p-0.5 select-none rounded ${opacity ? "opacity-60" : ""} ${up ? "bottom-10" : ""}`}>
+        <div className={`p-2.5 h-full rounded ${type === 0 ? "bg-gray-100" : type === 1 ? "bg-green-300" : type == 2 ? "bg-red-400" : ""}`}>
+        <AlertOctagon className={"absolute top-5 left-5 w-10 h-10 inline-block  "} />
+          <X onClick={handleClick} className={"absolute right-4 w-8 h-8 cursor-pointer"}/>
+          <p className={"absolute top-5 left-16 text-3xl font-bold ml-2"}>{title}</p>
+          <h1 className={"absolute top-20 left-7 text-xl font-bold"}>{msg}</h1>
+        </div>
+      </div>
+      )}
+    </>
+  );
+};
+
+export default Toast;
+export const myFunction = (param: any) => {
+
+};
+

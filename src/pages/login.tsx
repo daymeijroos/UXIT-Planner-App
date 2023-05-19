28d148e3e551201e26f078d90e8c278229d108cf
Day Meijroos <pekieend@gmail.com>
2023-05-19 15:32:31 +0200

    🩹: Import issues

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,3 @@
-import { NavigationBar } from "../components/aria/navigation-bar";
-import { DateSwitcher } from "../components/aria/date-switcher";
-import { Schedule } from "../components/aria/schedule";
-import { Button } from "../components/aria/button";
-import { Zap } from "react-feather";
 import LoginExample from "../components/example/login.example";
 
 const Index = () => {

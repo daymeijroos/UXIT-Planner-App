98eb3cf8b2fe4ca79433e9526e26495bfe234c7d
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-05-24 16:15:32 +0200

    init: added getAllUsers query

---
+++ server/api/userRouters/user.ts
@@ -0,0 +1,8 @@
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const userRouter = createTRPCRouter({
+  getAllUsers: publicProcedure
+    .query(({ ctx }) => {
+      return ctx.prisma.user.findMany({})
+    })
+})

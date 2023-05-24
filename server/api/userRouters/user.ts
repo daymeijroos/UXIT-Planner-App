233e9a5a6b6457f582594ad3746008c98ba365b2
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-05-24 16:16:05 +0200

    TODO: change publicProcedure to protectedProdcure when authorization is implemented

--- server/api/userRouters/user.ts
+++ server/api/userRouters/user.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
+  //TODO: protectedProcedure
   getAllUsers: publicProcedure
     .query(({ ctx }) => {
       return ctx.prisma.user.findMany({})

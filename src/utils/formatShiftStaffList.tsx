b1d15934fe395c0a79eea5f78a6718f329ee5b5b
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-05-25 11:54:57 +0200

    fix: typo

--- src/utils/formatShiftStaffList.tsx
+++ src/utils/formatShiftStaffList.tsx
@@ -10,7 +10,7 @@ export function formatShiftStaffList(staffing: StaffingWithColleagues) {
       shifts[shiftTypeName] = [];
     }
 
-    const staffName = `${nestedStaffing.user.first_name ?? 'Obekende naam'}`;
+    const staffName = `${nestedStaffing.user.first_name ?? 'Onbekende naam'}`;
 
     shifts[shiftTypeName].push(staffName);
   });

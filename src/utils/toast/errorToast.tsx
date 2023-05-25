2c935cc2e6662f51f8748eb5e3d8bd3211847bb2
Moatasem Alhumsi Alrefaai <54780624+mhtsm2001@users.noreply.github.com>
2023-05-25 13:18:47 +0200

    clean code

--- src/utils/toast/errorToast.tsx
+++ src/utils/toast/errorToast.tsx
@@ -1,14 +1,14 @@
 import toast from "react-hot-toast";
 import { AlertOctagon, X } from 'react-feather';
 
-export const errorToast = (message:string, darkmode:boolean) => {
+export const errorToast = (message:string) => {
 
   toast.custom((t) => (
     <>
-      {!darkmode ? (
-        <div className="bg-red-400 w-1/4 h-40 drop-shadow-xl border-black border-2">
+      {(
+        <div className="bg-red-400 w-1/4 h-40 drop-shadow-xl border-black border-2 dark:bg-red-600 dark:border-red-400">
           <div className="h-full absolute top-2 left-5">
-            <AlertOctagon className="w-12 h-12 text-black p-1 stroke-3" />
+            <AlertOctagon className="w-12 h-12 text-black p-1 stroke-3 dark:text-white" />
           </div>
           <div
             onClick={() => {
@@ -16,30 +16,12 @@ export const errorToast = (message:string, darkmode:boolean) => {
             }}
             className="cursor-pointer absolute top-1 right-2"
           >
-            <X className="w-10 h-10 text-black p-1 stroke-1" />
+            <X className="w-10 h-10 text-black p-1 stroke-1 dark:text-white" />
           </div>
           <div className="p-3 ml-16 mt-0.5">
-            <h2 className="font-bold text-3xl">Error</h2>
+            <h2 className="font-bold text-3xl dark:text-white">Error</h2>
           </div>
-          <p className="text-ellipsis p-3 ml-4 text-lg font-bold">{message}</p>
-        </div>
-      ) : (
-        <div className="bg-red-600 w-1/4 h-40 drop-shadow-xl border-red-400 border-2">
-          <div className="h-full absolute top-2 left-5">
-            <AlertOctagon className="w-12 h-12 text-white p-1 stroke-3" />
-          </div>
-          <div
-            onClick={() => {
-              toast.remove(t.id);
-            }}
-            className="cursor-pointer absolute top-1 right-2"
-          >
-            <X className="w-10 h-10 text-white p-1 stroke-1" />
-          </div>
-          <div className="p-3 ml-16 mt-0.5">
-            <h2 className="font-bold text-white text-3xl">Error</h2>
-          </div>
-          <p className="text-white text-ellipsis p-3 ml-4 text-lg font-bold">{message}</p>
+          <p className="text-ellipsis p-3 ml-4 text-lg font-bold dark:text-white">{message}</p>
         </div>
       )}
 
--- src/utils/toast/infoToast.tsx
+++ src/utils/toast/infoToast.tsx
@@ -1,14 +1,14 @@
 import toast from "react-hot-toast";
 import { Info, X } from 'react-feather';
 
-export const infoToast = (message:string, darkmode:boolean) => {
+export const infoToast = (message:string) => {
 
   toast.custom((t) => (
     <>
-      {!darkmode ? (
-        <div className="bg-zinc-50 w-1/4 h-40 drop-shadow-xl border-black border-2">
+      {(
+        <div className="bg-zinc-50 w-1/4 h-40 drop-shadow-xl border-black border-2 dark:bg-zinc-900 dark:border-zinc-400">
           <div className="h-full absolute top-2 left-5">
-            <Info className="w-12 h-12 text-black p-1 stroke-3" />
+            <Info className="w-12 h-12 text-black p-1 stroke-3 dark:text-white" />
           </div>
           <div
             onClick={() => {
@@ -16,33 +16,14 @@ export const infoToast = (message:string, darkmode:boolean) => {
             }}
             className="cursor-pointer absolute top-1 right-2"
           >
-            <X className="w-10 h-10 text-black p-1 stroke-1" />
+            <X className="w-10 h-10 text-black p-1 stroke-1 dark:text-white" />
           </div>
           <div className="p-3 ml-16 mt-0.5">
-            <h2 className="font-bold text-3xl">Info</h2>
+            <h2 className="font-bold text-3xl dark:text-white">Info</h2>
           </div>
-          <p className="text-ellipsis p-3 ml-4 text-lg font-bold">{message}</p>
-        </div>
-      ) : (
-        <div className="bg-zinc-900 w-1/4 h-40 drop-shadow-xl border-zinc-400 border-2">
-          <div className="h-full absolute top-2 left-5">
-            <Info className="w-12 h-12 text-white p-1 stroke-3" />
-          </div>
-          <div
-            onClick={() => {
-              toast.remove(t.id);
-            }}
-            className="cursor-pointer absolute top-1 right-2"
-          >
-            <X className="w-10 h-10 text-white p-1 stroke-1" />
-          </div>
-          <div className="p-3 ml-16 mt-0.5">
-            <h2 className="font-bold text-white text-3xl">Info</h2>
-          </div>
-          <p className="text-white text-ellipsis p-3 ml-4 text-lg font-bold">{message}</p>
+          <p className="text-ellipsis p-3 ml-4 text-lg font-bold dark:text-white">{message}</p>
         </div>
       )}
-
     </>
 
   ))

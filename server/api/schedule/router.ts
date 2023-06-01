3dd0ec7db85f96c7f26fe0330249d378e0e9759a
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-06-01 15:55:48 +0200

    fix: db query includes staffing and user from shift

--- server/api/schedule/router.ts
+++ server/api/schedule/router.ts
@@ -1,29 +1,29 @@
-import { z } from "zod"
+import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure, restrictedProcedure } from "../trpc"
-import { getUnfulfilledShifts } from "../../utils/getUnfulfilledShifts"
-import { Role } from "../../../prisma/role"
-import { generateSchedule } from "./generate-functions"
+import { createTRPCRouter, publicProcedure, restrictedProcedure } from "../trpc";
+import { getUnfulfilledShifts } from "../../utils/getUnfulfilledShifts";
+import { Role } from "../../../prisma/role";
+import { generateSchedule } from "./generate-functions";
 
 export const scheduleRouter = createTRPCRouter({
   getUnfulfilledShifts: publicProcedure
     .query(() => {
-      return getUnfulfilledShifts()
+      return getUnfulfilledShifts();
     }),
   generate: restrictedProcedure(Role.ADMIN)
     .input(z.object({
       from: z.date(),
-      to: z.date(),
+      to: z.date()
     }))
     .mutation(({ input }) => {
       return new Promise((resolve, reject) => {
         generateSchedule(input.from, input.to)
           .then(() => {
-            resolve('Success')
+            resolve("Success");
           })
           .catch((error) => {
-            reject(error)
-          })
-      })
+            reject(error);
+          });
+      });
     })
-})
+});
--- server/api/shift/router.ts
+++ server/api/shift/router.ts
@@ -3,6 +3,14 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 export const shiftRouter = createTRPCRouter({
   getAllShifts: protectedProcedure
     .query(({ ctx }) => {
-      return ctx.prisma.shift.findMany()
+      return ctx.prisma.shift.findMany({
+        include: {
+          staffings: {
+            include: {
+              user: true
+            }
+          }
+        }
+      });
     })
-})
+});

9323d0b22591bec7b41909c8c1bb3c3497aa7043
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-06-04 19:01:54 +0200

    styling: all buttons now follow Button template

--- src/pages/admin/aanpassen.tsx
+++ src/pages/admin/aanpassen.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react"
 import { Button, NavigationBar, ToastService } from "../../components"
 import { api } from "../../utils/api"
-import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { useAutoAnimate } from "@formkit/auto-animate/react"
 
 export default function Aanpassen() {
   const context = api.useContext()
-  const { mutate : removeSelectedStaffing } = api.staffing.removeStaffingAdmin.useMutation({
+  const { mutate: removeSelectedStaffing } = api.staffing.removeStaffingAdmin.useMutation({
     onSuccess: () => {
       context.shift.getAllShifts.invalidate().catch((reason) => {
-        console.log(reason);
+        console.log(reason)
+        ToastService.success("Het is gelukt!")
+      })
+    }
+  })
+  const { mutate: removeSelectedShift } = api.shift.removeShiftAdmin.useMutation({
+    onSuccess: () => {
+      context.shift.getAllShifts.invalidate().catch((reason) => {
+        console.log(reason)
         ToastService.success("Het is gelukt!")
       })
     }
@@ -33,7 +41,7 @@ export default function Aanpassen() {
       setExpandedRow(shiftId)
     }
   }
-  
+
   const handleRemoveStaffing = (staffingId: string) => {
     try {
       removeSelectedStaffing({ staffing_id: staffingId })
@@ -41,7 +49,15 @@ export default function Aanpassen() {
     } catch (error) {
       console.error(error)
     }
-    console.log(staffingId)
+  }
+
+  const handleRemoveShift = (shiftId: string) => {
+    try {
+      removeSelectedShift({ shift_id: shiftId })
+
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
@@ -65,7 +81,7 @@ export default function Aanpassen() {
             </th>
           </tr>
           </thead>
-          <tbody className="bg-white divide-y divide-gray-200" ref={tableRow}>
+          <tbody className="bg-white divide-y divide-gray-200">
           {shifts.data?.map((shift) => (
             <React.Fragment key={shift.id}>
               {expandedRow !== shift.id && (
@@ -96,31 +112,39 @@ export default function Aanpassen() {
                   <td colSpan="3">
                     <div className="p-4">
                       <div className="flex items-start justify-between mb-4">
-                        <div>
-                          <div className="flex items-center">
-                            <p>{shift.start.toString().slice(8, 10)} {shift.start.toString().slice(3, 7)} {shift.start.toString().slice(11, 15)}</p>
-                            <button className="px-2 py-1 bg-gray-200 rounded">Wijzig starttijd</button>
+                        <div className="flex flex-col">
+                          <div className="flex justify-between items-center max-w-xs mb-2">
+                            <p className="mr-6">{shift.start.toString().slice(8, 10)} {shift.start.toString().slice(3, 7)} {shift.start.toString().slice(11, 15)} {shift.start.toString().slice(16, 21)}</p>
+                            <div className="w-40">
+                              <Button color="gray">Wijzig starttijd</Button>
+                            </div>
                           </div>
-                          <div className="flex items-center mt-2">
-                            <p>{shift.end.toString().slice(8, 10)} {shift.end.toString().slice(3, 7)} {shift.end.toString().slice(11, 15)}</p>
-                            <button className="px-2 py-1 bg-gray-200 rounded">Wijzig eindtijd</button>
+                          <div className="flex justify-between items-center max-w-xs mb-2">
+                            <p className="mr-6">{shift.end.toString().slice(8, 10)} {shift.end.toString().slice(3, 7)} {shift.end.toString().slice(11, 15)} {shift.start.toString().slice(16, 21)}</p>
+                            <div className="w-40">
+                              <Button color="gray">Wijzig eindtijd</Button>
+                            </div>
                           </div>
                         </div>
-                        <Button className="px-4 py-2 bg-red-500 text-white rounded">Verwijder shift</Button>
+                        <div className="w-40 ml-4">
+                          <Button color="red" onPress={() => handleRemoveShift(shift.id)}>Verwijder shift</Button>
+                        </div>
                       </div>
-                      <div className="flex flex-col mb-4" ref={parent}>
+                      <div className="flex flex-col mb-4" ref={staffingList}>
                         {shift.staffings.map((staffing) => (
-                          <div key={staffing.id} className="flex justify-between max-w-xs mb-2">
+                          <div key={staffing.id} className="flex justify-between items-center max-w-xs mb-2">
                             <p>{staffing.user.name} {staffing.user.last_name}</p>
-                            <div className="w-24">
+                            <div className="w-32">
                               <Button color="red" onPress={() => handleRemoveStaffing(staffing.id)}>Verwijder</Button>
                             </div>
                           </div>
                         ))}
                       </div>
-                      <div className="flex justify-start">
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded">Voeg staffing toe</button>
-                      </div>
+                      {/*<div className="flex justify-center">*/}
+                        <div className="">
+                        <Button color="teal">Voeg staffing toe</Button>
+                        </div>
+                      {/*</div>*/}
                     </div>
                   </td>
                 </tr>

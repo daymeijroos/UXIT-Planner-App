6a6534d50165603143a0383ba0e687005ebca161
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-06-05 00:40:09 +0200

    feat: added getUsersWithPreferencesAndStaffings method to user/router.ts

--- server/api/user/router.ts
+++ server/api/user/router.ts
@@ -1,8 +1,31 @@
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { getUsersWithPreferencesAndStaffings } from "./database-actions";
 
 export const userRouter = createTRPCRouter({
   getAllUsers: protectedProcedure
     .query(({ ctx }) => {
-      return ctx.prisma.user.findMany({})
+      return ctx.prisma.user.findMany({});
+    }),
+  getUsersWithPreferencesAndStaffings: protectedProcedure
+    .query(({ ctx }) => {
+      return ctx.prisma.user.findMany({
+        include: {
+          preference: {
+            include: {
+              absence: true,
+              availability_week: {
+                include: {
+                  availability: {
+                    include: {
+                      shift_types: true
+                    }
+                  }
+                }
+              }
+            }
+          },
+          staffings: true
+        }
+      });
     })
 })

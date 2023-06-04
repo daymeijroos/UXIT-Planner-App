d7ade47ad15a0212f72acc62aecc8d13e3dcd568
maxdevr <102964846+maxdevr@users.noreply.github.com>
2023-06-04 23:35:02 +0200

    fix: staffings, staff_required and Backup are now deleted upon cascade of shift deletion

--- server/api/shift/router.ts
+++ server/api/shift/router.ts
@@ -24,30 +24,6 @@ export const shiftRouter = createTRPCRouter({
       shift_id: z.string()
     }))
     .mutation(async ({ ctx, input }) => {
-      await ctx.prisma.staffing.deleteMany({
-        where: {
-          shift: {
-            id: input.shift_id,
-          },
-        },
-      });
-
-      await ctx.prisma.staff_required.deleteMany({
-        where: {
-          shift: {
-            id: input.shift_id,
-          },
-        },
-      });
-
-      // await ctx.prisma.backup.deleteMany({
-      //   where: {
-      //     shift: {
-      //       id: shift_id,
-      //     },
-      //   },
-      // });
-
       return ctx.prisma.shift.delete({
         where: {
           id: input.shift_id

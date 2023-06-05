dffe6736f54c0a615e4032cb9a71716ed69c6c85
Moatasem Alhumsi Alrefaai <54780624+mhtsm2001@users.noreply.github.com>
2023-06-05 12:22:10 +0200

    Working Selector (light, dark) mode v1.0

---
+++ src/components/atoms/input/ListBox.tsx
@@ -0,0 +1,100 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import * as React from "react";
+import type { AriaListBoxOptions } from "@react-aria/listbox";
+import type { ListState } from "react-stately";
+import type { Node } from "@react-types/shared";
+import { useListBox, useListBoxSection, useOption } from "react-aria";
+
+interface ListBoxProps extends AriaListBoxOptions<unknown> {
+  listBoxRef?: React.RefObject<HTMLUListElement>;
+  state: ListState<unknown>;
+}
+
+interface SectionProps {
+  section: Node<unknown>;
+  state: ListState<unknown>;
+}
+
+interface OptionProps {
+  item: Node<unknown>;
+  state: ListState<unknown>;
+}
+
+export function ListBox(props: ListBoxProps) {
+  let ref = React.useRef<HTMLUListElement>(null);
+  let { listBoxRef = ref, state } = props;
+  let { listBoxProps } = useListBox(props, state, listBoxRef);
+
+  return (
+    <ul
+      {...listBoxProps}
+      ref={listBoxRef}
+      className="w-full max-h-72 overflow-auto outline-none"
+    >
+      {[...state.collection].map((item) =>
+        item.type === "section" ? (
+          <ListBoxSection key={item.key} section={item} state={state} />
+        ) : (
+          <Option key={item.key} item={item} state={state} />
+        )
+      )}
+    </ul>
+  );
+}
+
+function ListBoxSection({ section, state }: SectionProps) {
+  let { itemProps, headingProps, groupProps } = useListBoxSection({
+    heading: section.rendered,
+    "aria-label": section["aria-label"]
+  });
+
+  return (
+    <>
+      <li {...itemProps} className="pt-2">
+        {section.rendered && (
+          <span
+            {...headingProps}
+            className="text-xs font-bold uppercase text-gray-500 mx-3"
+          >
+            {section.rendered}
+          </span>
+        )}
+        <ul {...groupProps}>
+          {[...section.childNodes].map((node) => (
+            <Option key={node.key} item={node} state={state} />
+          ))}
+        </ul>
+      </li>
+    </>
+  );
+}
+
+function Option({ item, state }: OptionProps) {
+  let ref = React.useRef<HTMLLIElement>(null);
+  let { optionProps, isDisabled, isSelected, isFocused } = useOption(
+    {
+      key: item.key
+    },
+    state,
+    ref
+  );
+
+  let text = "text-gray-700 dark:text-white";
+  if (isFocused || isSelected) {
+    text = "ring-2 ring-yellow-500 text-black";
+  } else if (isDisabled) {
+    text = "text-gray-200 dark:text-white";
+  }
+
+  return (
+    <li
+      {...optionProps}
+      ref={ref}
+      className={`m-1 rounded-sm py-2 px-2 text-sm outline-none cursor-pointer flex items-center justify-between ${text} ${
+        isFocused ? "bg-yellow-100" : ""
+      } ${isSelected ? "font-bold dark:text-white focus:text-black" : ""}`}
+    >
+      {item.rendered}
+    </li>
+  );
+}
---
+++ src/components/atoms/input/Selector.tsx
@@ -0,0 +1,109 @@
+import * as React from "react";
+import type { AriaSelectProps } from "@react-types/select";
+import { useSelectState } from "react-stately";
+import { ChevronDown, ChevronUp } from 'react-feather'
+
+import {
+  useSelect,
+  HiddenSelect,
+  useButton,
+  mergeProps,
+  useFocusRing
+} from "react-aria";
+
+import { ListBox } from "./ListBox";
+import { Popover } from "./Popover";
+
+export { Item } from "react-stately";
+
+
+
+export function Select<T extends object>(props: AriaSelectProps<T>) {
+  // Create state based on the incoming props
+  let state = useSelectState(props);
+
+  // Get props for child elements from useSelect
+  let ref = React.useRef(null);
+  let { labelProps, triggerProps, valueProps, menuProps } = useSelect(
+    props,
+    state,
+    ref
+  );
+
+  // Get props for the button based on the trigger props from useSelect
+  let { buttonProps } = useButton(triggerProps, ref);
+
+  let { focusProps, isFocusVisible } = useFocusRing();
+
+  return (
+    <div className="inline-flex flex-col relative w-72 mt-4">
+      <div
+        {...labelProps}
+        className="block text-sm font-medium text-gray-700 dark:text-white text-left cursor-default mb-1"
+      >
+        {props.label}
+      </div>
+      <HiddenSelect
+        state={state}
+        triggerRef={ref}
+        label={props.label}
+        name={props.name}
+      />
+      <button
+        {...mergeProps(buttonProps, focusProps)}
+        ref={ref}
+        className={`p-1 pl-3 py-1 relative inline-flex flex-row items-center justify-between rounded-sm overflow-hidden cursor-default shadow-sm border-r-2 border-l-2 border-t-2 border-b-4 outline-none ${
+          isFocusVisible ? "dark:border-gray-400 border-black ring-4 ring-yellow-500" : "dark:border-gray-400 border-black"
+        } ${state.isOpen ? "bg-gray-100 dark:bg-gray-600" : "dark:bg-gray-700 bg-white"}`}
+      >
+        <span
+          {...valueProps}
+          className={`text-md ${
+            state.selectedItem ? "dark:text-white text-black" : "dark:text-gray-400 text-gray-600"
+          }`}
+        >
+          {state.selectedItem
+            ? state.selectedItem.rendered
+            : "Kies een medewerker/Vrijwilliger"}
+        </span>
+        <>
+          {!state.isOpen && (
+            <ChevronDown
+              className={`w-5 h-5 ${isFocusVisible ? "dark:text-white text-black" : "dark:text-white text-black"}`}
+            />
+          )}
+        </>
+
+        <>
+          {state.isOpen && (
+            <ChevronUp
+              className={`w-5 h-5 ${isFocusVisible ? "dark:text-white text-black" : "dark:text-white text-black"}`}
+            />
+          )}
+        </>
+
+      </button>
+      {state.isOpen && (
+        <Popover
+          state={state}
+          triggerRef={ref}
+          placement="bottom start"
+          className="w-72"
+        >
+          <ListBox {...menuProps} state={state} />
+        </Popover>
+      )}
+    </div>
+  );
+}
+
+
+// Template:
+// <Select label="Favorite Animals">
+//   <Item key="red panda">Red Panda</Item>
+//   <Item key="cat">Cat</Item>
+//   <Item key="dog">Dog</Item>
+//   <Item key="aardvark">Aardvark</Item>
+//   <Item key="kangaroo">Kangaroo</Item>
+//   <Item key="snake">Snake</Item>
+// </Select>

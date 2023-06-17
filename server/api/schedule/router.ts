0057fd31cb41e0c06d66cebc44665fb30eaaab85
wardalbam <wardalbam32@gmail.com>
2023-06-17 02:40:15 +0200

    added backend shift endpoints

--- server/api/schedule/router.ts
+++ server/api/schedule/router.ts
@@ -4,6 +4,7 @@ import { createTRPCRouter, publicProcedure, restrictedProcedure } from "../trpc"
 import { getUnfulfilledShifts } from "../../utils/getUnfulfilledShifts"
 import { Role } from "../../../prisma/role"
 import { generateSchedule } from "./generate-functions"
+import { prisma } from "../../db"
 
 export const scheduleRouter = createTRPCRouter({
   getUnfulfilledShifts: publicProcedure
@@ -25,5 +26,74 @@ export const scheduleRouter = createTRPCRouter({
             reject(error)
           })
       })
-    })
+    }),
+
+    getShiftTypes: publicProcedure
+    .query(() => {
+        return prisma.shift_Type.findMany({
+            select: {
+                name: true
+            }
+        })
+    }
+    ),
+    // return true if  shift by date and starttime 
+    checkShift: publicProcedure
+    .input(z.object({
+        date: z.date(),
+        startTime: z.string()
+    }))
+    .query(async ({ input }) => {
+        const start = 
+        new Date(input.date.getFullYear(), input.date.getMonth(), input.date.getDate(), parseInt(input.startTime.split(':')[0]), parseInt(input.startTime.split(':')[1]))
+        const shift = await prisma.shift.findFirst({
+            where: {
+                start: start
+            }
+        })
+        if (shift) {
+            return true
+        } else {
+            return false
+        }
+    }),
+
+   
+    createShift : publicProcedure
+    .input(
+      z.object({
+        start: z.date(),
+        end: z.date(),
+        staff_required: z.array(
+          z.object({
+            shift_type: z.string(),
+            amount: z.number(),
+          })
+        ),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const shift = await prisma.shift.create({
+        data: {
+          start: input.start,
+          end: input.end,
+          staff_required: {
+            create: input.staff_required.map(({ shift_type, amount }) => ({
+              amount,
+              shift_type: {
+                connect: {
+                  name: shift_type,
+                },
+              },
+            })),
+          },
+        },
+      });
+      return shift;
+      }),
+
+      // get all shifts
+      getAllShifts : publicProcedure
+      .query(()=>prisma.shift.findMany()),
 })
+
